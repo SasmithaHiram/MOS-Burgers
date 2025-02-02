a7d45fa6648a7ddea6b342975d58ec7717d8da84
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-03 03:11:57 +0530

    Login Completed

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,16 @@
 const adminUsername = "Sasmitha";
 const adminPassword = "123";
 
+
 function loginPOS() {
     let userUsername = document.getElementById("username").value;
     let userPassword = document.getElementById("password").value;
 
-    if (userUsername === null) {
-        alert("Text Field Is Empty");
+    if (userUsername === "" || userPassword === "") {
+        alert("Please fill  in both fields.")
+    } else if (userUsername === adminUsername && userPassword === adminPassword) {
+        alert("Heellloo");
+    } else {
+        alert("Invalid username or password");
     }
 }
