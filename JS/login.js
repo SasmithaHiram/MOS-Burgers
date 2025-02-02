d76aa2f713c4e9317e0ca4fc1fc826995048b4fe
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-03 02:41:45 +0530

    first commit

---
+++ JS/login.js
@@ -0,0 +1,11 @@
+const adminUsername = "Sasmitha";
+const adminPassword = "123";
+
+function loginPOS() {
+    let userUsername = document.getElementById("username").value;
+    let userPassword = document.getElementById("password").value;
+
+    if (userUsername === null) {
+        alert("Text Field Is Empty");
+    }
+}
