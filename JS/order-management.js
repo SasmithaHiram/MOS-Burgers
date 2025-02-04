490866272aaf1bb2d5b94f075c1c9a4d699055c5
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-04 22:32:44 +0530

    JSON Update

--- JS/order-management.js
+++ JS/order-management.js
@@ -1,70 +1,69 @@
 let menu = {
     "burgers": [
-        {"id": "B1001", "name": "Classic Burger (Large)", "price": "Rs.750.00", "discount": "Discount : 0%", "image" : "/Assets/Classic Burger (Large).webp"},
-        {"id": "B1002", "name": "Classic Burger (Regular)", "price": "Rs.1500.00", "discount": "Discount : 15%", "image" : "/Assets/Classic Burger (Regular).jpeg"},
-        {"id": "B1003", "name": "Turkey Burger", "price": "Rs.1600.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1004", "name": "Chicken Burger (Large)", "price": "Rs.1400.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1005", "name": "Chicken Burger (Regular)", "price": "Rs.800.00", "discount": "Discount : 20%", "image" : ""},
-        {"id": "B1006", "name": "Cheese Burger (Large)", "price": "Rs.1000.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1007", "name": "Cheese Burger (Regular)", "price": "Rs.600.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1008", "name": "Bacon Burger", "price": "Rs.650.00", "discount": "Discount : 15%", "image" : ""},
-        {"id": "B1009", "name": "Shawarma Burger", "price": "Rs.800.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1010", "name": "Olive Burger", "price": "Rs.1800.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1012", "name": "Double-Cheese Burger", "price": "Rs.1250.00", "discount": "Discount : 20%", "image" : ""},
-        {"id": "B1013", "name": "Crispy Chicken Burger (Regular)", "price": "Rs.1200.00", "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1014", "name": "Crispy Chicken Burger (Large)", "price": "Rs.1600.00", "discount": "Discount :1 0%", "image" : ""},
-        {"id": "B1015", "name": "Paneer Burger", "price": "Rs.900.00", "discount": "Discount : 0%", "image" : ""}
+        { "id": "B1001", "name": "Classic Burger (Large)", "price": "Rs.750.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1002", "name": "Classic Burger (Regular)", "price": "Rs.1500.00", "discount": "Discount : 15%", "image": "/Assets/Classic Burger (Regular).jpeg" },
+        { "id": "B1003", "name": "Turkey Burger", "price": "Rs.1600.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1004", "name": "Chicken Burger (Large)", "price": "Rs.1400.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1005", "name": "Chicken Burger (Regular)", "price": "Rs.800.00", "discount": "Discount : 20%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1006", "name": "Cheese Burger (Large)", "price": "Rs.1000.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1007", "name": "Cheese Burger (Regular)", "price": "Rs.600.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1008", "name": "Bacon Burger", "price": "Rs.650.00", "discount": "Discount : 15%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1009", "name": "Shawarma Burger", "price": "Rs.800.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1010", "name": "Olive Burger", "price": "Rs.1800.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1012", "name": "Double-Cheese Burger", "price": "Rs.1250.00", "discount": "Discount : 20%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1013", "name": "Crispy Chicken Burger (Regular)", "price": "Rs.1200.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1014", "name": "Crispy Chicken Burger (Large)", "price": "Rs.1600.00", "discount": "Discount :1 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1015", "name": "Paneer Burger", "price": "Rs.900.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" }
     ],
     "submarines": [
-        {"id": "B1016", "name": "Crispy Chicken Submarine (Large)", "price": 2000.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1017", "name": "Crispy Chicken Submarine (Regular)", "price": 1500.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1018", "name": "Chicken Submarine (Large)", "price": 1800.00, "discount": "Discount : 3%", "image" : ""},
-        {"id": "B1019", "name": "Chicken Submarine (Regular)", "price": 1400.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1020", "name": "Grinder Submarine", "price": 2300.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1021", "name": "Cheese Submarine", "price": 2200.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1022", "name": "Double Cheese n Chicken Submarine", "price": 1900.00, "discount": "Discount : 16%", "image" : ""},
-        {"id": "B1023", "name": "Special Horgie Submarine", "price": 2800.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1024", "name": "MOS Special Submarine", "price": 3000.00, "discount": "Discount : 0%", "image" : ""}
+        { "id": "B1016", "name": "Crispy Chicken Submarine (Large)", "price": 2000.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1017", "name": "Crispy Chicken Submarine (Regular)", "price": 1500.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1018", "name": "Chicken Submarine (Large)", "price": 1800.00, "discount": "Discount : 3%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1019", "name": "Chicken Submarine (Regular)", "price": 1400.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1020", "name": "Grinder Submarine", "price": 2300.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1021", "name": "Cheese Submarine", "price": 2200.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1022", "name": "Double Cheese n Chicken Submarine", "price": 1900.00, "discount": "Discount : 16%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1023", "name": "Special Horgie Submarine", "price": 2800.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1024", "name": "MOS Special Submarine", "price": 3000.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" }
     ],
     "fries": [
-        {"id": "B1025", "name": "Steak Fries (Large)", "price": 1200.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1026", "name": "Steak Fries (Medium)", "price": 600.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1027", "name": "French Fries (Large)", "price": 800.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1028", "name": "French Fries (Medium)", "price": 650.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1029", "name": "French Fries (Small)", "price": 450.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1030", "name": "Sweet Potato Fries (Large)", "price": 600.00, "discount": "Discount : 0%", "image" : ""}
+        { "id": "B1025", "name": "Steak Fries (Large)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1026", "name": "Steak Fries (Medium)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1027", "name": "French Fries (Large)", "price": 800.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1028", "name": "French Fries (Medium)", "price": 650.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1029", "name": "French Fries (Small)", "price": 450.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1030", "name": "Sweet Potato Fries (Large)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" }
     ],
     "pasta": [
-        {"id": "B1031", "name": "Chicken n Cheese Pasta", "price": 1600.00, "discount": "Discount : 15%", "image" : ""},
-        {"id": "B1032", "name": "Chicken Penne Pasta", "price": 1700.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1033", "name": "Ground Turkey Pasta Bake", "price": 2900.00, "discount": "Discount : 10%", "image" : ""},
-        {"id": "B1034", "name": "Creamy Shrimp Pasta", "price": 2000.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1035", "name": "Lemon Butter Pasta", "price": 1950.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1036", "name": "Tagliatelle Pasta", "price": 2400.00, "discount": "Discount : 1%", "image" : ""},
-        {"id": "B1037", "name": "Baked Ravioli", "price": 2000.00, "discount": "Discount : 1%", "image" : ""}
+        { "id": "B1031", "name": "Chicken n Cheese Pasta", "price": 1600.00, "discount": "Discount : 15%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1032", "name": "Chicken Penne Pasta", "price": 1700.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1033", "name": "Ground Turkey Pasta Bake", "price": 2900.00, "discount": "Discount : 10%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1034", "name": "Creamy Shrimp Pasta", "price": 2000.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1035", "name": "Lemon Butter Pasta", "price": 1950.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1036", "name": "Tagliatelle Pasta", "price": 2400.00, "discount": "Discount : 1%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1037", "name": "Baked Ravioli", "price": 2000.00, "discount": "Discount : 1%", "image": "/Assets/Chicken n Cheese Pasta.webp" }
     ],
     "chicken": [
-        {"id": "B1038", "name": "Fried Chicken (Small)", "price": 1200.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1039", "name": "Fried Chicken (Regular)", "price": 2300.00, "discount": "Discount : 10%", "image" : ""},
-        {"id": "B1040", "name": "Fried Chicken (Large)", "price": 3100.00, "discount": "Discount : 5%", "image" : ""},
-        {"id": "B1041", "name": "Hot Wings (Large)", "price": 2400.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1042", "name": "Devilled Chicken (Large)", "price": 900.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1043", "name": "BBQ Chicken (Regular)", "price": 2100.00, "discount": "Discount : 0%", "image" : ""}
+        { "id": "B1038", "name": "Fried Chicken (Small)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1039", "name": "Fried Chicken (Regular)", "price": 2300.00, "discount": "Discount : 10%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1040", "name": "Fried Chicken (Large)", "price": 3100.00, "discount": "Discount : 5%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1041", "name": "Hot Wings (Large)", "price": 2400.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1042", "name": "Devilled Chicken (Large)", "price": 900.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1043", "name": "BBQ Chicken (Regular)", "price": 2100.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" }
     ],
     "beverages": [
-        {"id": "B1044", "name": "Pepsi (330ml)", "price": 990.00, "discount": "Discount : 5%", "image" : ""},
-        {"id": "B1045", "name": "Coca-Cola (330ml)", "price": 1230.00, "discount": "Discount : 0%", "image" : ""},
-        {"id": "B1046", "name": "Sprite (330ml)", "price": 1500.00, "discount": "Discount : 3%", "image" : ""},
-        {"id": "B1047", "name": "Mirinda (330ml)", "price": 850.00, "discount": "Discount : 7%", "image" : ""}
+        { "id": "B1044", "name": "Pepsi (330ml)", "price": 990.00, "discount": "Discount : 5%", "image": "/Assets/Pepsi (330ml).avif" },
+        { "id": "B1045", "name": "Coca-Cola (330ml)", "price": 1230.00, "discount": "Discount : 0%", "image": "/Assets/Pepsi (330ml).avif" },
+        { "id": "B1046", "name": "Sprite (330ml)", "price": 1500.00, "discount": "Discount : 3%", "image": "/Assets/Pepsi (330ml).avif" },
+        { "id": "B1047", "name": "Mirinda (330ml)", "price": 850.00, "discount": "Discount : 7%", "image": "/Assets/Pepsi (330ml).avif" }
     ]
+
 };
-
-
 
 function loadAllMenu() {
     const menuTable = document.getElementById("card");
     Object.keys(menu).forEach(category => {
-        
+
         menu[category].forEach(item => {
             menuTable.innerHTML += `<div class="col-md-3 mb-4">
     <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
@@ -76,10 +75,8 @@ function loadAllMenu() {
             <p id="itemDiscount1" class="text-warning fw-bold">${item.discount}</p>
         </div>
     </div>
-</div>`            
+</div>`
         });
-        
-        
-        
     });
+
 }
