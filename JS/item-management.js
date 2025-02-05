044b0b056964920bbca54a5d3cade3ba14f9ecf1
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-05 13:52:28 +0530

    Item Management Completed

--- JS/item-management.js
+++ JS/item-management.js
@@ -1,16 +1,96 @@
-let burgers = [];
+const menu = {
+    "burgers": [
+        { "id": "B1001", "name": "Classic Burger (Large)", "price": 750.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1002", "name": "Classic Burger (Regular)", "price": 1500.00, "discount": "Discount : 15%", "image": "/Assets/Classic Burger (Regular).jpeg" },
+        { "id": "B1003", "name": "Turkey Burger", "price": 1600.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1004", "name": "Chicken Burger (Large)", "price": 1400.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1005", "name": "Chicken Burger (Regular)", "price": 800.00, "discount": "Discount : 20%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1006", "name": "Cheese Burger (Large)", "price": 1000.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1007", "name": "Cheese Burger (Regular)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1008", "name": "Bacon Burger", "price": 650.00, "discount": "Discount : 15%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1009", "name": "Shawarma Burger", "price": 800.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1010", "name": "Olive Burger", "price": 1800.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1012", "name": "Double-Cheese Burger", "price": 1250.00, "discount": "Discount : 20%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1013", "name": "Crispy Chicken Burger (Regular)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1014", "name": "Crispy Chicken Burger (Large)", "price": 1600.00, "discount": "Discount :1 0%", "image": "/Assets/Classic Burger (Large).webp" },
+        { "id": "B1015", "name": "Paneer Burger", "price": 900.00, "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" }
+    ],
+    "submarines": [
+        { "id": "B1016", "name": "Crispy Chicken Submarine (Large)", "price": 2000.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1017", "name": "Crispy Chicken Submarine (Regular)", "price": 1500.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1018", "name": "Chicken Submarine (Large)", "price": 1800.00, "discount": "Discount : 3%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1019", "name": "Chicken Submarine (Regular)", "price": 1400.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1020", "name": "Grinder Submarine", "price": 2300.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1021", "name": "Cheese Submarine", "price": 2200.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1022", "name": "Double Cheese n Chicken Submarine", "price": 1900.00, "discount": "Discount : 16%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1023", "name": "Special Horgie Submarine", "price": 2800.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
+        { "id": "B1024", "name": "MOS Special Submarine", "price": 3000.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" }
+    ],
+    "fries": [
+        { "id": "B1025", "name": "Steak Fries (Large)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1026", "name": "Steak Fries (Medium)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1027", "name": "French Fries (Large)", "price": 800.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1028", "name": "French Fries (Medium)", "price": 650.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1029", "name": "French Fries (Small)", "price": 450.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
+        { "id": "B1030", "name": "Sweet Potato Fries (Large)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" }
+    ],
+    "pasta": [
+        { "id": "B1031", "name": "Chicken n Cheese Pasta", "price": 1600.00, "discount": "Discount : 15%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1032", "name": "Chicken Penne Pasta", "price": 1700.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1033", "name": "Ground Turkey Pasta Bake", "price": 2900.00, "discount": "Discount : 10%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1034", "name": "Creamy Shrimp Pasta", "price": 2000.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1035", "name": "Lemon Butter Pasta", "price": 1950.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1036", "name": "Tagliatelle Pasta", "price": 2400.00, "discount": "Discount : 1%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
+        { "id": "B1037", "name": "Baked Ravioli", "price": 2000.00, "discount": "Discount : 1%", "image": "/Assets/Chicken n Cheese Pasta.webp" }
+    ],
+    "chicken": [
+        { "id": "B1038", "name": "Fried Chicken (Small)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1039", "name": "Fried Chicken (Regular)", "price": 2300.00, "discount": "Discount : 10%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1040", "name": "Fried Chicken (Large)", "price": 3100.00, "discount": "Discount : 5%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1041", "name": "Hot Wings (Large)", "price": 2400.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1042", "name": "Devilled Chicken (Large)", "price": 900.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
+        { "id": "B1043", "name": "BBQ Chicken (Regular)", "price": 2100.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" }
+    ],
+    "beverages": [
+        { "id": "B1044", "name": "Pepsi (330ml)", "price": 990.00, "discount": "Discount : 5%", "image": "/Assets/Pepsi (330ml).avif" },
+        { "id": "B1045", "name": "Coca-Cola (330ml)", "price": 1230.00, "discount": "Discount : 0%", "image": "/Assets/Pepsi (330ml).avif" },
+        { "id": "B1046", "name": "Sprite (330ml)", "price": 1500.00, "discount": "Discount : 3%", "image": "/Assets/Pepsi (330ml).avif" },
+        { "id": "B1047", "name": "Mirinda (330ml)", "price": 850.00, "discount": "Discount : 7%", "image": "/Assets/Pepsi (330ml).avif" }
+    ]
+
+};
+
+loadItemTable();
 
 function addBurger() {
-    let itemCode = document.getElementById("code").value;
-    let itemName = document.getElementById("name").value;
-    let itemPrice = document.getElementById("price").value;
-    let itemDescription = document.getElementById("description").value;
+    let id = document.getElementById("code").value;
+    let name = document.getElementById("name").value;
+    let price = document.getElementById("price").value;
+    let discount = document.getElementById("discount").value;
+    let itemImage = document.getElementById("image").files[0];
+    let image = itemImage ? URL.createObjectURL(itemImage) : "";
+    let category = document.getElementById("select").value;
 
-    if (itemCode === "" || itemName === "" || itemPrice === "" || itemDescription === "") {
+    if (id === "" || name === "" || price === "" || discount === "" || image === "") {
         alert("All fields are required!");
+        return;
+    } if (category == "Burgers") {
+        menu.burgers.push({ id, name, price, discount, image });
+    } else if (category == "Submarines") {
+        menu.submarines.push({ id, name, price, discount, image });
+    } else if (category == "Fries") {
+        menu.fries.push({ id, name, price, discount, image });
+    }
+    else if (category == "Pasta") {
+        menu.pasta.push({ id, name, price, discount, image });
+    }
+    else if (category == "Chicken") {
+        menu.chicken.push({ id, name, price, discount, image });
+    }
+    else if (category == "Beverages") {
+        menu.beverages.push({ id, name, price, discount, image });
     }
 
-    burgers.push({ itemCode, itemName, itemPrice, itemDescription });
     loadItemTable();
 
 }
@@ -19,38 +99,42 @@ function loadItemTable() {
     const burgerItemList = document.getElementById("table-body");
     burgerItemList.innerHTML = "";
 
-    burgers.forEach((burger, index) => {
-        const row = document.createElement("tr");
+    Object.keys(menu).forEach(category => {
+        menu[category].forEach((item, index) => {
+            const row = document.createElement("tr");
 
-        row.innerHTML = `<th scope="row">${burger.itemCode}</th>
-      <td>${burger.itemName}</td>
-      <td>${burger.itemPrice}</td>
-      <td>${burger.itemDescription}</td>
-      <td>
-      <button class="btn-edit" onclick="editBurger(${index})">Edit</button>
-      <button class="btn-delete" onclick="deleteBurger(${index})">Delete</button>
-    </td>`;
+            row.innerHTML = `<th scope="row">${item.id}</th>
+              <td>${item.name}</td>
+              <td>${item.price}</td>
+               <td>${item.discount}</td>
+               <td><img src="${item.image}" alt="image" height="100" width="100"></td>
+              <td>
+              <button class="btn-edit" onclick="editItems('${category}', ${index})">Edit</button>
+              <button class="btn-delete" onclick="deleteItems('${category}', ${index})">Delete</button>
+            </td>`;
 
-        burgerItemList.appendChild(row);
+            burgerItemList.appendChild(row);
+        });
     });
 
 }
 
-function editBurger(index) {
-    const burger = burgers[index];
-    
-    document.getElementById("code").value = burger.itemCode;
-    document.getElementById("name").value = burger.itemName;
-    document.getElementById("price").value = burger.itemPrice;
-    document.getElementById("description").value = burger.itemDescription;
+function editItems(category, index) {
+    const item = menu[category][index];
 
-    burgers.splice(index, 1);
+    document.getElementById("code").value = item.id;
+    document.getElementById("name").value = item.name;
+    document.getElementById("price").value = item.price;
+    document.getElementById("image").value = "";
+    document.getElementById("discount").value = item.discount;
+
+    menu[category].splice(index, 1);
     loadItemTable();
 
 }
 
-function deleteBurger(index) {
-    burgers.splice(index, 1);
+function deleteItems(category, index) {
+    menu[category].splice(index, 1);
     loadItemTable();
 
 }
--- JS/order-management.js
+++ JS/order-management.js
@@ -1,67 +1,10 @@
-let menu = {
-    "burgers": [
-        { "id": "B1001", "name": "Classic Burger (Large)", "price": "Rs.750.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1002", "name": "Classic Burger (Regular)", "price": "Rs.1500.00", "discount": "Discount : 15%", "image": "/Assets/Classic Burger (Regular).jpeg" },
-        { "id": "B1003", "name": "Turkey Burger", "price": "Rs.1600.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1004", "name": "Chicken Burger (Large)", "price": "Rs.1400.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1005", "name": "Chicken Burger (Regular)", "price": "Rs.800.00", "discount": "Discount : 20%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1006", "name": "Cheese Burger (Large)", "price": "Rs.1000.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1007", "name": "Cheese Burger (Regular)", "price": "Rs.600.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1008", "name": "Bacon Burger", "price": "Rs.650.00", "discount": "Discount : 15%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1009", "name": "Shawarma Burger", "price": "Rs.800.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1010", "name": "Olive Burger", "price": "Rs.1800.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1012", "name": "Double-Cheese Burger", "price": "Rs.1250.00", "discount": "Discount : 20%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1013", "name": "Crispy Chicken Burger (Regular)", "price": "Rs.1200.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1014", "name": "Crispy Chicken Burger (Large)", "price": "Rs.1600.00", "discount": "Discount :1 0%", "image": "/Assets/Classic Burger (Large).webp" },
-        { "id": "B1015", "name": "Paneer Burger", "price": "Rs.900.00", "discount": "Discount : 0%", "image": "/Assets/Classic Burger (Large).webp" }
-    ],
-    "submarines": [
-        { "id": "B1016", "name": "Crispy Chicken Submarine (Large)", "price": 2000.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1017", "name": "Crispy Chicken Submarine (Regular)", "price": 1500.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1018", "name": "Chicken Submarine (Large)", "price": 1800.00, "discount": "Discount : 3%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1019", "name": "Chicken Submarine (Regular)", "price": 1400.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1020", "name": "Grinder Submarine", "price": 2300.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1021", "name": "Cheese Submarine", "price": 2200.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1022", "name": "Double Cheese n Chicken Submarine", "price": 1900.00, "discount": "Discount : 16%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1023", "name": "Special Horgie Submarine", "price": 2800.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" },
-        { "id": "B1024", "name": "MOS Special Submarine", "price": 3000.00, "discount": "Discount : 0%", "image": "/Assets/Crispy Chicken Submarine (Large).jpg" }
-    ],
-    "fries": [
-        { "id": "B1025", "name": "Steak Fries (Large)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
-        { "id": "B1026", "name": "Steak Fries (Medium)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
-        { "id": "B1027", "name": "French Fries (Large)", "price": 800.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
-        { "id": "B1028", "name": "French Fries (Medium)", "price": 650.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
-        { "id": "B1029", "name": "French Fries (Small)", "price": 450.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" },
-        { "id": "B1030", "name": "Sweet Potato Fries (Large)", "price": 600.00, "discount": "Discount : 0%", "image": "/Assets/Steak Fries (Large).jpg" }
-    ],
-    "pasta": [
-        { "id": "B1031", "name": "Chicken n Cheese Pasta", "price": 1600.00, "discount": "Discount : 15%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
-        { "id": "B1032", "name": "Chicken Penne Pasta", "price": 1700.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
-        { "id": "B1033", "name": "Ground Turkey Pasta Bake", "price": 2900.00, "discount": "Discount : 10%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
-        { "id": "B1034", "name": "Creamy Shrimp Pasta", "price": 2000.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
-        { "id": "B1035", "name": "Lemon Butter Pasta", "price": 1950.00, "discount": "Discount : 0%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
-        { "id": "B1036", "name": "Tagliatelle Pasta", "price": 2400.00, "discount": "Discount : 1%", "image": "/Assets/Chicken n Cheese Pasta.webp" },
-        { "id": "B1037", "name": "Baked Ravioli", "price": 2000.00, "discount": "Discount : 1%", "image": "/Assets/Chicken n Cheese Pasta.webp" }
-    ],
-    "chicken": [
-        { "id": "B1038", "name": "Fried Chicken (Small)", "price": 1200.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
-        { "id": "B1039", "name": "Fried Chicken (Regular)", "price": 2300.00, "discount": "Discount : 10%", "image": "/Assets/Fried Chicken (Small).jpg" },
-        { "id": "B1040", "name": "Fried Chicken (Large)", "price": 3100.00, "discount": "Discount : 5%", "image": "/Assets/Fried Chicken (Small).jpg" },
-        { "id": "B1041", "name": "Hot Wings (Large)", "price": 2400.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
-        { "id": "B1042", "name": "Devilled Chicken (Large)", "price": 900.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" },
-        { "id": "B1043", "name": "BBQ Chicken (Regular)", "price": 2100.00, "discount": "Discount : 0%", "image": "/Assets/Fried Chicken (Small).jpg" }
-    ],
-    "beverages": [
-        { "id": "B1044", "name": "Pepsi (330ml)", "price": 990.00, "discount": "Discount : 5%", "image": "/Assets/Pepsi (330ml).avif" },
-        { "id": "B1045", "name": "Coca-Cola (330ml)", "price": 1230.00, "discount": "Discount : 0%", "image": "/Assets/Pepsi (330ml).avif" },
-        { "id": "B1046", "name": "Sprite (330ml)", "price": 1500.00, "discount": "Discount : 3%", "image": "/Assets/Pepsi (330ml).avif" },
-        { "id": "B1047", "name": "Mirinda (330ml)", "price": 850.00, "discount": "Discount : 7%", "image": "/Assets/Pepsi (330ml).avif" }
-    ]
 
-};
+const menuTable = document.getElementById("card");
+
+loadAllMenu();
+
 
 function loadAllMenu() {
-    const menuTable = document.getElementById("card");
     Object.keys(menu).forEach(category => {
 
         menu[category].forEach(item => {
@@ -77,6 +20,113 @@ function loadAllMenu() {
     </div>
 </div>`
         });
+    });
+
+}
+
+function loadBurgers() {
+    menuTable.innerHTML = "";
+    menu.burgers.forEach(burger => {
+        menuTable.innerHTML += `<div class="col-md-3 mb-4">
+    <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
+        <img id="itemImage1" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;" src="${burger.image}" alt="Menu Item">
+        <div class="card-body text-center">
+            <h5 id="itemName1" class="card-id">${burger.id}</h5>
+            <h5 id="itemName2" class="card-name">${burger.name}</h5>
+            <p id="itemPrice1" class="text-primary fw-bold fs-5">${burger.price}</p>
+            <p id="itemDiscount1" class="text-warning fw-bold">${burger.discount}</p>
+        </div>
+    </div>
+</div>`
+    });
+
+}
+
+function loadSubmarines() {
+    menuTable.innerHTML = "";
+    menu.submarines.forEach(submarine => {
+        menuTable.innerHTML += `<div class="col-md-3 mb-4">
+    <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
+        <img id="itemImage1" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;" src="${submarine.image}" alt="Menu Item">
+        <div class="card-body text-center">
+            <h5 id="itemName1" class="card-id">${submarine.id}</h5>
+            <h5 id="itemName2" class="card-name">${submarine.name}</h5>
+            <p id="itemPrice1" class="text-primary fw-bold fs-5">${submarine.price}</p>
+            <p id="itemDiscount1" class="text-warning fw-bold">${submarine.discount}</p>
+        </div>
+    </div>
+</div>`
+    });
+
+}
+
+function loadFries() {
+    menuTable.innerHTML = "";
+    menu.fries.forEach(frie => {
+        menuTable.innerHTML += `<div class="col-md-3 mb-4">
+    <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
+        <img id="itemImage1" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;" src="${frie.image}" alt="Menu Item">
+        <div class="card-body text-center">
+            <h5 id="itemName1" class="card-id">${frie.id}</h5>
+            <h5 id="itemName2" class="card-name">${frie.name}</h5>
+            <p id="itemPrice1" class="text-primary fw-bold fs-5">${frie.price}</p>
+            <p id="itemDiscount1" class="text-warning fw-bold">${frie.discount}</p>
+        </div>
+    </div>
+</div>`
+    });
+
+}
+
+function loadPastas() {
+    menuTable.innerHTML = "";
+    menu.pasta.forEach(pasta => {
+        menuTable.innerHTML += `<div class="col-md-3 mb-4">
+    <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
+        <img id="itemImage1" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;" src="${pasta.image}" alt="Menu Item">
+        <div class="card-body text-center">
+            <h5 id="itemName1" class="card-id">${pasta.id}</h5>
+            <h5 id="itemName2" class="card-name">${pasta.name}</h5>
+            <p id="itemPrice1" class="text-primary fw-bold fs-5">${pasta.price}</p>
+            <p id="itemDiscount1" class="text-warning fw-bold">${pasta.discount}</p>
+        </div>
+    </div>
+</div>`
+    });
+
+}
+
+function loadChickens() {
+    menuTable.innerHTML = "";
+    menu.chicken.forEach(chicken => {
+        menuTable.innerHTML += `<div class="col-md-3 mb-4">
+    <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
+        <img id="itemImage1" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;" src="${chicken.image}" alt="Menu Item">
+        <div class="card-body text-center">
+            <h5 id="itemName1" class="card-id">${chicken.id}</h5>
+            <h5 id="itemName2" class="card-name">${chicken.name}</h5>
+            <p id="itemPrice1" class="text-primary fw-bold fs-5">${chicken.price}</p>
+            <p id="itemDiscount1" class="text-warning fw-bold">${chicken.discount}</p>
+        </div>
+    </div>
+</div>`
+    });
+}
+
+function loadBeverages() {
+    menuTable.innerHTML = "";
+    menu.beverages.forEach(beverage => {
+        menuTable.innerHTML += `<div class="col-md-3 mb-4">
+    <div class="card shadow-lg w-100" style="max-width: 300px; height: 400px">
+        <img id="itemImage1" class="card-img-top" style="width: 100%; height: 200px; object-fit: cover;" src="${beverage.image}" alt="Menu Item">
+        <div class="card-body text-center">
+            <h5 id="itemName1" class="card-id">${beverage.id}</h5>
+            <h5 id="itemName2" class="card-name">${beverage.name}</h5>
+            <p id="itemPrice1" class="text-primary fw-bold fs-5">${beverage.price}</p>
+            <p id="itemDiscount1" class="text-warning fw-bold">${beverage.discount}</p>
+        </div>
+    </div>
+</div>`
     });
 
 }
