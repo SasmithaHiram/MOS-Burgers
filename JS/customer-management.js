8890fedbd420b79a93b1380063f56894db64eb02
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-24 08:18:30 +0530

    Done Update Customer

--- JS/customer-management.js
+++ JS/customer-management.js
@@ -1,82 +1,115 @@
 function addCustomer() {
-    let customerName = document.getElementById("name").value;
-    let customerEmail = document.getElementById("email").value;
-    let customerPhoneNumber = document.getElementById("phone-number").value;
+  let customerName = document.getElementById("name").value;
+  let customerEmail = document.getElementById("email").value;
+  let customerPhoneNumber = document.getElementById("phone-number").value;
 
-    if (customerName === "" || customerEmail === "" || customerPhoneNumber === "") {
-        alert("All fields are required!");
-    }
+  if (
+    customerName === "" ||
+    customerEmail === "" ||
+    customerPhoneNumber === ""
+  ) {
+    alert("All fields are required!");
+    return;
+  }
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify({
-        "name": customerName,
-        "email": customerEmail,
-        "phoneNumber": customerPhoneNumber
-    });
+  const raw = JSON.stringify({
+    name: customerName,
+    email: customerEmail,
+    phoneNumber: customerPhoneNumber,
+  });
 
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    fetch("http://localhost:8080/customer/add-customer", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
+  fetch("http://localhost:8080/customer/add-customer", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 
-    clearCustomer();
+  clearCustomer();
 }
 
 loadCustomersTable();
 
 function loadCustomersTable() {
-    const requestOptions = {
-        method: "GET",
-        redirect: "follow"
-    };
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-    fetch("http://localhost:8080/customer/get-all-customers", requestOptions)
-        .then((response) => response.json())
-        .then((result) => {
-            const customerTable = document.getElementById("table-body");
-            customerTable.innerHTML = "";
+  fetch("http://localhost:8080/customer/get-all-customers", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      const customerTable = document.getElementById("table-body");
+      customerTable.innerHTML = "";
 
-            result.forEach((customer) => {
-                const row = document.createElement("tr");
+      result.forEach((customer) => {
+        const row = document.createElement("tr");
 
-                row.innerHTML = `
+        row.innerHTML = `
         <th scope="row">${customer.id}</th>
       <td>${customer.name}</td>
       <td>${customer.email}</td>
       <td>${customer.phoneNumber}</td>
       <td>
-       <button class="btn-edit btn btn-primary" onclick="editCustomer(${customer.id})">Edit</button>
-    <button class="btn-delete btn btn-danger" onclick="deleteCustomer(${customer.id})">Delete</button>
+       <button class="btn-edit btn btn-primary" onclick="editCustomer('${customer.id}', '${customer.name}', '${customer.email}', '${customer.phoneNumber}')">Edit</button>
+    <button class="btn-delete btn btn-danger" onclick="deleteCustomer(${customer})">Delete</button>
      </td>`;
 
-                customerTable.appendChild(row);
-
-            });
-        })
-        .catch((error) => console.error(error));
+        customerTable.appendChild(row);
+      });
+    })
+    .catch((error) => console.error(error));
 }
 
-// function editCustomer(index) {
-//     const customer = customers[index];
+let currentCustomerId = null;
 
-//     document.getElementById("id").value = customer.customerId;
-//     document.getElementById("name").value = customer.customerName;
-//     document.getElementById("email").value = customer.customerEmail;
-//     document.getElementById("phone-number").value = customer.customerPhoneNumber;
+function editCustomer(id, name, email, phoneNumber) {
+  currentCustomerId = id;
+  document.getElementById("name").value = name;
+  document.getElementById("email").value = email;
+  document.getElementById("phone-number").value = phoneNumber;
 
-//     customers.splice(index, 1);
-//     loadCustomersTable();
+  document.getElementById("add-button").innerHTML = "Update";
+  document
+    .getElementById("add-button")
+    .setAttribute("onclick", "updateCustomer()");
+}
 
-// }
+function updateCustomer() {
+  let customerName = document.getElementById("name").value;
+  let customerEmail = document.getElementById("email").value;
+  let customerPhoneNumber = document.getElementById("phone-number").value;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    id: currentCustomerId,
+    name: customerName,
+    email: customerEmail,
+    phoneNumber: customerPhoneNumber,
+  });
+
+  const requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:8080/customer/update-customer", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+}
 
 // function deleteCustomer(index) {
 //     customers.splice(index, 1);
@@ -85,7 +118,8 @@ function loadCustomersTable() {
 // }
 
 function clearCustomer() {
-    let customerName = document.getElementById("name").value = "";
-    let customerEmail = document.getElementById("email").value = "";
-    let customerPhoneNumber = document.getElementById("phone-number").value = "";
+  let customerName = (document.getElementById("name").value = "");
+  let customerEmail = (document.getElementById("email").value = "");
+  let customerPhoneNumber = (document.getElementById("phone-number").value =
+    "");
 }
